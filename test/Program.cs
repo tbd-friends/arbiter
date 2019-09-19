17a38bfa0f5080f46b6dbd6253ae247ff799adde
Terry Burns-Dyson <terrybd@gmail.com>
2019-09-18 21:11:30 -0400

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security;
+
+namespace test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var arbiter = new Arbiter();
+
+            arbiter.Send(new AddNumbersCommand()
+            {
+                Id = 1,
+                Name = "Test",
+                ActionDateTime = DateTime.UtcNow
+            });
+
+            arbiter.Send<AddNumbersCommand>(c =>
+            {
+                c.Id = 1;
+                c.Name = "Test";
+                c.ActionDateTime = DateTime.UnixEpoch;
+            });
+
+        }
+    }
+
+    public class AddNumbersCommand
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ActionDateTime { get; set; }
+    }
+
+}
